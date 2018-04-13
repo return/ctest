ebd5e893ef0261b6fc7b4fe015f1b2bb8f6c4a68
Travis CI User <travis@example.org>
2018-04-13 19:12:51 +0000

    Documentation for alexcrichton/ctest@94815ca

---
+++ implementors/core/marker/trait.Send.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["ctest"] = [{text:"impl !<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"ctest/struct.TestGenerator.html\" title=\"struct ctest::TestGenerator\">TestGenerator</a>",synthetic:true,types:["ctest::TestGenerator"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
